ab3670d01b758a73774f02088df231037a5bc046
12node12 <101336871+12node12@users.noreply.github.com>
2022-03-10 18:41:12 +0530

    Add files via upload

---
+++ nwe_file.py
@@ -0,0 +1,30 @@
+import urllib.request
+import os
+import requests
+base_url='https://www.mybidfood.com.au/api/s_v1/Image/GetImage/'
+for i in range(1,100):
+    image_id=str(62167)+ str(i)
+    print(image_id)
+
+    full_url='https://www.mybidfood.com.au/api/s_v1/Image/GetImage/'+image_id
+
+    print(full_url)
+    image_name='fulldata/'+image_id+'.jpg'
+
+    page = requests.get(full_url)
+    # print(page.content)
+
+    # urllib.request.urlretrieve(full_url, image_name)
+    with open(image_name, 'wb') as f:
+        f.write(page.content)
+
+
+
+
+# url = 'https://www.mybidfood.com.au/api/s_v1/Image/GetImage/6216741'
+# page = requests.get(url)
+#
+# f_ext = os.path.splitext(url)[-1]
+# f_name = 'img{}'.format(f_ext)
+# with open(f_name, 'wb') as f:
+#     f.write(page.content)
